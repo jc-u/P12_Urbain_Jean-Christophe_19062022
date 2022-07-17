3d07091545ea4fa0d120ea77031d39baed54d946
Jean-Christophe Urbain <jcurbain.admin@gmail.com>
2022-07-17 15:09:40 +0200

    Updates

---
+++ precache-manifest.69e04ed18092d017c68bb068434202fa.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "95aad41165b5dc687dfe",
+    "url": "/P12_Urbain_Jean-Christophe_19062022/static/css/main.2d3e634e.chunk.css"
+  },
+  {
+    "revision": "95aad41165b5dc687dfe",
+    "url": "/P12_Urbain_Jean-Christophe_19062022/static/js/main.95aad411.chunk.js"
+  },
+  {
+    "revision": "b7626dce4fbe5fd6ee4a",
+    "url": "/P12_Urbain_Jean-Christophe_19062022/static/js/1.b7626dce.chunk.js"
+  },
+  {
+    "revision": "0802f89d676072b72add",
+    "url": "/P12_Urbain_Jean-Christophe_19062022/static/js/runtime~main.0802f89d.js"
+  },
+  {
+    "revision": "9194b5a87b81a2b56bdeec8abcefe0d5",
+    "url": "/P12_Urbain_Jean-Christophe_19062022/static/media/icon1.9194b5a8.svg"
+  },
+  {
+    "revision": "15bda523f68472df19891488fa94059a",
+    "url": "/P12_Urbain_Jean-Christophe_19062022/static/media/icon2.15bda523.svg"
+  },
+  {
+    "revision": "f60d29ede793b08c9b038b3a01f5b3c9",
+    "url": "/P12_Urbain_Jean-Christophe_19062022/static/media/icon3.f60d29ed.svg"
+  },
+  {
+    "revision": "676e3f7b3f7ba46477dbd41195e511a4",
+    "url": "/P12_Urbain_Jean-Christophe_19062022/static/media/icon4.676e3f7b.svg"
+  },
+  {
+    "revision": "606a97bccd26352696a5871e4136f8d5",
+    "url": "/P12_Urbain_Jean-Christophe_19062022/static/media/logo.606a97bc.svg"
+  },
+  {
+    "revision": "6e4294b2b9952cc7139c0c2d116d8173",
+    "url": "/P12_Urbain_Jean-Christophe_19062022/static/media/fat-icon.6e4294b2.svg"
+  },
+  {
+    "revision": "b6097ceee0aad0961eb55673e820a37d",
+    "url": "/P12_Urbain_Jean-Christophe_19062022/static/media/carbs-icon.b6097cee.svg"
+  },
+  {
+    "revision": "fec6c31058b29d2e60bb37aa4381b787",
+    "url": "/P12_Urbain_Jean-Christophe_19062022/static/media/calories-icon.fec6c310.svg"
+  },
+  {
+    "revision": "882174f3c9a6010aebff6616ab0c74f3",
+    "url": "/P12_Urbain_Jean-Christophe_19062022/static/media/protein-icon.882174f3.svg"
+  },
+  {
+    "revision": "7a9898a7a169428bc383c7b6d095e905",
+    "url": "/P12_Urbain_Jean-Christophe_19062022/index.html"
+  }
+];
